5459008c8bf1a8148b59d03603c611e43fa9abf5
pkeogh1024@yahoo.com <Tiesha Sims>
2016-12-20 16:15:03 -0700

    added delete

--- public/js/app.js
+++ public/js/app.js
@@ -17,24 +17,29 @@ app.config([
                 // template:'<h2>Home Page</h2>'
                 templateUrl: '/templates/home.html',
                 controller: 'MyApp.HomeController'
-        })
+            })
             .state ('about', {
                 url: '/about',
                 // template: '<h2>About Page</h2>'
                 templateUrl: '/templates/about.html'
-        })
+            })
             .state ('contact',{
                 url: '/contact',
                 // template: '<h2>Contact Page</h2>'
                 templateUrl: '/templates/contact.html'
-        })
+            })
+            .state ('request',{
+                url: '/request',
+                templateUrl: '/templates/request.html',
+                controller: 'MyApp.HttpRequestController'
+            })
         ;
     }
-])
+]);
+
 
 //Create a home page controller
-app.controller('MyApp.HomeController', [
-    //List the dependencies for this controller
+app.controller ('MyApp.HomeController',[
     '$scope',
 
     //Provide a call back to run once all the
@@ -56,8 +61,93 @@ app.controller('MyApp.HomeController', [
         $scope.addListItem = function() {
             console.log ('Add List Item');
             $scope.nameList.push ('Bob');
+
+
         }
 
 
+    }
+]);
+
+app.controller ('MyApp.HttpRequestController',[
+    '$scope', '$http',
+
+    function ($scope, $http) {
+        console.log ('RequestController')
+
+        $scope.post = {};
+        $scope.postList = [];
+
+        $scope.create = function () {
+            console.log ('Trying to create a post:', $scope.post)
+
+            $http ({
+                url: 'http://localhost:3000/posts',
+                //Use the "POST" method because we want to
+                //post/ create data on the server.
+                method: 'POST',
+                data: $scope.post,
+
+            })
+            .success (function (response) {
+                console.log ('This is the error:', response);
+            })
+            .error (function (response){
+                console.error ('This is the error: ', response);
+            })
+        }
+        $scope.readAll = function () {
+            console.log ('Reading all the post');
+
+            //Make a call to grab at the post objects
+            $http ({
+                url: 'http://localhost:3000/posts',
+                //Use the "POST" method because we want to
+                //post/ create data on the server.
+                method: 'GET',
+                data: $scope.post
+            })
+            .success (function (response) {
+                console.log ('This is the response:', response);
+                $scope.postList = response;
+            })
+            .error (function (response){
+                console.log ('This is the error: ', response);
+            })
+        }
+        $scope.update = function (){
+            console.log ("Updating Posts");
+            $http ({
+                url: 'http://localhost:3000/posts/' + $scope.post.id,
+                //Use the "POST" method because we want to
+                //post/ create data on the server.
+                method: 'PUT',
+                data: $scope.post,
+
+            })
+            .success (function (response) {
+                console.log ('This is the error:', response);
+            })
+            .error (function (response){
+                console.error ('This is the error: ', response);
+            })
+        }
+        $scope.delete = function (){
+            console.log ("Updating Posts");
+            $http ({
+                url: 'http://localhost:3000/posts/' + $scope.post.id,
+                //Use the "POST" method because we want to
+                //post/ create data on the server.
+                method: 'DELETE',
+                data: $scope.post,
+
+            })
+            .success (function (response) {
+                console.log ('This is the error:', response);
+            })
+            .error (function (response){
+                console.error ('This is the error: ', response);
+            })
+        }
     }
 ]);
