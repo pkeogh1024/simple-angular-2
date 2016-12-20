5387a6df2a6ebec0f25108c8dd6552a3204d194c
pkeogh1024@yahoo.com <Tiesha Sims>
2016-12-20 11:37:37 -0700

    added controller and scope, added navigation

---
+++ public/js/app.js
@@ -0,0 +1,57 @@
+'use strict'
+
+var app =angular.module ('MyApp', ['ui.router']);
+
+//Configure the 'MyApp' angular application.
+app.config([
+    //Bring in the dependency objects by name.
+    '$stateProvider',
+
+    //Provide a callback function to use one all
+    //the dependencies have been loaded.
+    function ($stateProvider) {
+        //Setup and define out page states.
+        $stateProvider
+            .state ('home',{
+                url: '/',
+                // template:'<h2>Home Page</h2>'
+                templateUrl: '/templates/home.html',
+                controller: 'MyApp.HomeController'
+        })
+            .state ('about', {
+                url: '/about',
+                // template: '<h2>About Page</h2>'
+                templateUrl: '/templates/about.html'
+        })
+            .state ('contact',{
+                url: '/contact',
+                // template: '<h2>Contact Page</h2>'
+                templateUrl: '/templates/contact.html'
+        })
+        ;
+    }
+])
+
+//Create a home page controller
+app.controller('MyApp.HomeController', [
+    //List the dependencies for this controller
+    '$scope',
+
+    //Provide a call back to run once all the
+    //dependencies have been created.
+    function ($scope) {
+        console.log('The home controller has loaded.');
+
+        //The scope object acts as a pubic area that is available to
+        //mark up pageand other objects or services
+        $scope.name = 'Bob';
+        $scope.age = 32;
+
+        $scope.increaseAge = function() {
+            console.log ('Increasing the age.');
+            $scope.age = $scope.age+1;
+        }
+
+
+    }
+]);
